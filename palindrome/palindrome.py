029e79a551afa55191a7c626d6b2658f6b9aad04
sergey <karpofs@mail.ru>
2022-02-01 00:14:36 +0300

    [upd]

---
+++ palindrome/palindrome.py
@@ -0,0 +1,32 @@
+# Compare string with itself reversed
+def is_palindrome_1(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+
+# Recursive deleting edges
+def is_palindrome_2(s):
+    if s == '':
+        return True
+    if s[0] == s[len(s)-1]:
+        return is_palindrome_2(s[1:len(s)-1])
+    else:
+        return False
+
+
+# Human readable output
+def output(func, strs):
+    print(f'\n---Function "{func.__name__}" output---\n')
+    for s in strs:
+        neg = ' not'
+        if func(''.join(s.split()).lower()): neg = ''
+        print(f'"{s}" is{neg} a palindrome')
+
+
+with open('palindrome/test_strings.txt') as test_file:
+    strings = test_file.read().split('\n')
+
+output(is_palindrome_1, strings)
+output(is_palindrome_2, strings)
+
